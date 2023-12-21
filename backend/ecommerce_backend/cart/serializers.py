290f30adc062c6fb283541b73f94a77350fbed97
MaRu0205 <53577662+MaRu0205@users.noreply.github.com>
2023-12-21 09:14:39 +0100

    added cart endpoints

--- backend/ecommerce_backend/cart/serializers.py
+++ backend/ecommerce_backend/cart/serializers.py
@@ -1,19 +1,28 @@
 from rest_framework import serializers
 from .models import Cart, CartItem
-from product.serializers import SizeSerializer, ColorSerializer
+from product.models import Product, Size, Color
 
 class CartItemSerializer(serializers.ModelSerializer):
-    size = SizeSerializer(read_only=True)
-    color = ColorSerializer(read_only=True)
+    product = serializers.PrimaryKeyRelatedField(queryset=Product.objects.all())
+    size = serializers.PrimaryKeyRelatedField(queryset=Size.objects.all(), allow_null=True)
+    color = serializers.PrimaryKeyRelatedField(queryset=Color.objects.all(), allow_null=True)
 
     class Meta:
         model = CartItem
-        fields = ['id', 'product', 'quantity', 'size', 'color']
+        fields = ['product', 'quantity', 'size', 'color']
+
 
 class CartSerializer(serializers.ModelSerializer):
-    items = CartItemSerializer(many=True, read_only=True)
+    items = CartItemSerializer(many=True)
 
     class Meta:
         model = Cart
-        fields = ['id', 'user', 'items', 'created_at']
+        fields = ['id', 'items']
+
+    def create(self, validated_data):
+        items_data = validated_data.pop('items')
+        cart = Cart.objects.create(**validated_data)
+        for item_data in items_data:
+            CartItem.objects.create(cart=cart, **item_data)
+        return cart
 
